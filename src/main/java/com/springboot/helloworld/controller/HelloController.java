73e3d00df0561726ce1517a98754eb0445923876
Madhu <102540730+madhu-aala@users.noreply.github.com>
2026-04-02 19:24:37 +0530

    Initial spring boot hello world application setup

---
+++ src/main/java/com/springboot/helloworld/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.springboot.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String getHelloMessage() {
+	    return "Hello Spring Boot!";
+	}
+}
